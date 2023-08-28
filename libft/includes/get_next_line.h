aa602f80b626adaf49c0f2a6d00f447b925a8c20
du than <du.than@relexsolutions.com>
2023-08-28 10:53:29 +0300

    changed 3 to 1000 buffer

--- libft/includes/get_next_line.h
+++ libft/includes/get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: dthan <dthan@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 01:39:52 by dthan             #+#    #+#             */
-/*   Updated: 2023/08/28 00:14:42 by dthan            ###   ########.fr       */
+/*   Updated: 2023/08/28 10:52:59 by dthan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 3
+# define BUFF_SIZE 1000
 # define FD_MAX 256
 # include <stdlib.h>
 # include <fcntl.h>
